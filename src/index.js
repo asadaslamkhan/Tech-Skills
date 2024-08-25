4447b57d29805dba568605fe3f9656cbd19bf946
Asad Aslam <opworrier@gmail.com>
2024-08-26 01:02:52 +0500

    upadtion

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 function sendMail() {
     var params = {
         name: document.getElementById("name").value,
+        phone: document.getElementById("phone no").value,
         email: document.getElementById("email").value,
-        phone: document.getElementById("phone").value,
-        subject: document.getElementById("subject").value,
+        subject: document.getElementById("Subject").value,
         message: document.getElementById("message").value,
     };
-    const serviceID = "service_nvw9j28";
+    const serviceID = "service_qk25k2e";
     const templateID = "template_id5zqd7"
 
     emailjs.send(serviceID, templateID, params)
         .then(
             res => {
                 document.getElementById("name").value = "";
+                document.getElementById("phone no").value = "";
                 document.getElementById("email").value = "";
-                document.getElementById("phone").value = "";
                 document.getElementById("subject").value = "";
                 document.getElementById("message").value = "";
                 console.log(res);
